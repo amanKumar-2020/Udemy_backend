c8a0a18b9c5b5e04d63766acf0308200e9ff627e
Aman Kumar <jnvaman4@gmail.com>
2025-09-17 00:21:52 +0530

    auth user login

--- Auth_session/routes/user.route.js
+++ Auth_session/routes/user.route.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { usersTable } from "../db/schema.js";
-import db from "../db/index.js"
+import db from "../db/index.js";
 
 import { eq } from "drizzle-orm";
 
 import { randomBytes, createHmac } from "node:crypto";
-import { table } from "node:console";
+
 
 const router = express.Router();
 
-router.get('/',async (req,res)=>{
+router.get("/", async (req, res) => {
   return res.status(200).json({
-    message:`user route is also running`
-  })
-})
+    message: `user route is also running`,
+  });
+});
 
 router.post("/signup", async (req, res) => {
   const { name, password, email } = req.body;
@@ -22,7 +22,7 @@ router.post("/signup", async (req, res) => {
     .select({ email: usersTable.email })
     .from(usersTable)
     .where((table) => eq(table.email, email));
-  
+
   if (exitingUser) {
     return res
       .status(400)
@@ -32,25 +32,53 @@ router.post("/signup", async (req, res) => {
   const hashedPassword = createHmac("sha256", salt)
     .update(password)
     .digest("hex");
-    
+
   const [user] = await db
     .insert(usersTable)
     .values({ name, email, password: hashedPassword, salt })
     .returning({ id: usersTable.id });
 
   return res.status(201).json({
-    status:"success" , data: {userId: user.id}
-  })
-
+    status: "success",
+    data: { userId: user.id },
+  });
 });
 
-// router.post("/login",async (req,res)=>{
-//   const { password, email } = req.body;
-//   const [exitingUser] =await db
-//   .select()
-//   .from(usersTable)
-//   .where((table)=> eq())
+router.post("/login", async (req, res) => {
+  const { password, email } = req.body;
+  const [exitingUser] = await db
+    .select({
+      email: usersTable.email,
+      salt: usersTable.salt,
+      password: usersTable.password,
+      id : usersTable.id,
+    })
+    .from(usersTable)
+    .where((table) => eq(table.email, email));
 
-// })
+  if (!exitingUser) {
+    return res.status(401).json({
+      status: "error",
+      message: "Invalid credentials",
+    });
+  }
+  if (exitingUser) {
+  
+      const newHashed = createHmac("sha256", exitingUser.salt)
+       .update(password)
+       .digest("hex");
+
+
+
+      if(newHashed!==exitingUser.password){
+        return res.status(401).json({
+          status: "error",
+          message: "Invalid credentials",
+        });
+      }else{
+        return res.json({ status: "success", userId: exitingUser.id });
+      }
+    }
+});
 
 export default router;
